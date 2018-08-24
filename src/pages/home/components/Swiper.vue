2d59b0e29ae019afd08a4e71148f72903d21f443
lvchaoyang <960278791@qq.com>
2018-08-24 13:17:13 +0800

    change

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -5,36 +5,36 @@
       <swiper-slide v-for="item in swiperList" :key="item.id">
         <img class="swiper-img" :src="item.imgUrl">
       </swiper-slide>
-      <div class="swiper-pagination"  slot="pagination"></div>
+      <div class="swiper-pagination" slot="pagination"></div>
     </swiper>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'HomeSwiper',
-    data () {
-      return {
-        swiperOption: {
-          pagination: '.swiper-pagination',
-          loop: true
-        },
-        swiperList: [{
-          id: '0001',
-          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1807/4f/37f83b87138a0c02.jpg_750x200_8e5df442.jpg'
-        }, {
-          id: '0002',
-          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1806/c5/8f384b861e4e9e02.jpg_750x200_29d574ba.jpg'
-        }, {
-          id: '0003',
-          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1807/66/e5a5cec881702f02.jpg_750x200_67bb5691.jpg'
-        }, {
-          id: '0004',
-          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1808/18/2dbce82b07a23402.jpg_750x200_14356b03.jpg'
-        }]
-      }
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1807/4f/37f83b87138a0c02.jpg_750x200_8e5df442.jpg'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1806/c5/8f384b861e4e9e02.jpg_750x200_29d574ba.jpg'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1807/66/e5a5cec881702f02.jpg_750x200_67bb5691.jpg'
+      }, {
+        id: '0004',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1808/18/2dbce82b07a23402.jpg_750x200_14356b03.jpg'
+      }]
     }
   }
+}
 </script>
 
 <style scoped>
@@ -44,9 +44,11 @@
     height: 0;
     padding-bottom: 31.25%;
   }
+
   .swiper-img {
     width: 100%;
   }
+
   .wrapper >>> .swiper-pagination-bullet-active {
     background: #ffffff;
   }
