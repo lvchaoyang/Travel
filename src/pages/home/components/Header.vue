edd1c739e0a8bf2f8dc67c1e948499467d70de0b
lvchaoyang <960278791@qq.com>
2018-08-27 15:06:10 +0800

    add city-router

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -5,11 +5,14 @@
       </div>
       <div class="header-input">
         <span class="iconfont">&#xe632;</span>
-        请输入城市/景点/游玩主题</div>
-      <div class="header-right">
-        {{this.city}}
-        <span class="iconfont arrow-icon">&#xe64a;</span>
+        请输入城市/景点/游玩主题
       </div>
+      <router-link to="/city">
+        <div class="header-right">
+          {{this.city}}
+          <span class="iconfont arrow-icon">&#xe64a;</span>
+        </div>
+      </router-link>
     </div>
 </template>
 
@@ -53,6 +56,7 @@ export default {
     width: 1.24rem;
     float: right;
     text-align: center;
+    color: #ffffff;
   }
   .arrow-icon {
     margin-left: -0.04rem;
